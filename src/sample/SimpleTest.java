92b8bce0a78431ac71ce575b159f70ee4cc9759d
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-12-22 09:40:22 +0100

    oprettede javaFx projekt med opgave et og delt på github

---
+++ src/sample/SimpleTest.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class SimpleTest {
+
+    // Opgave 1 SimpleTest
+    public static void main(String[] args) {
+        // print til consol gennem sout metoden.
+        System.out.println("Velkommen til min opskrifts-applikations-dummy");
+
+    }
+
+
+
+    // Opgave 2 hjælpe Metode til at vise blanke linjer
+
+    public void blankLinje(){
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
